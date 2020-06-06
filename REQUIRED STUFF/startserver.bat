705ca261003b9594468a8a5ae4cd171df84977f8
Debasish Nandi <debasishfairytail@gmail.com>
2020-06-06 21:33:53 +0530

    commit 8 : animeBUFF atlast added the OTHER_names module... did some additional stuff like adding extraPick_1 as wallpaper and fixing the trailervideo section in the animeTitle.html page such that if will be collapsed when first opened... the video has a smooth opening finish [nearly] ... ADDITIONALLY {CREATED A SCRIPTGENERATOR APPLICATION FOR THE EASY TO USE AND OPENING OF THE SERVER WHEN CORRECT OPERATIONS ARE APPLIED}  MINOR FIXING OF THE STABILITY IS STILL REQUIRED, ...  Trailer video is fixed IN models ... need to push a good complete code and save it that way... Still need to add the hints and work on the Start page of the application ... need to make documentaion for the new folks who will use my app ... MAKE IT A WAY THAT A NON CODER MAY ALSO USE IT EASILY WITH MINOR BUGS AND ATTENTIONS TO WORK...

--- REQUIRED STUFF/startserver.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-cd D:\programming\Tutorial Django\AnimeBUFF
-
-python manage.py runserver
-
-PAUSE
--- animetitles/models.py
+++ animetitles/models.py
@@ -4,16 +4,19 @@ import re
 
 
 # GLOBAL_server_root_Address = "http://192.168.137.1:8887/"
-GLOBAL_server_root_Address = "http://127.0.0.1:8887/"
+# GLOBAL_server_root_Address = "http://localhost:8887/"
+GLOBAL_server_root_Address = "http://127.0.0.1:11111/"
+# GLOBAL_server_root_Address = "http://127.0.0.1:8887/"
 
 
 # Create your models here.
 class AnimeTitle(models.Model):
     title =  models.CharField(max_length=100)
     profile = models.ImageField(upload_to = "images/")
+    otherNames = models.CharField(max_length=200)
     genres = models.CharField(max_length=200)
     summery =  models.TextField()
-    trailer = models.CharField(max_length=170)
+    trailer = models.CharField(max_length=170 , blank = True)
     # put blank=True within the brackets
     extrapick_1 = models.ImageField(upload_to = "images/",
                     blank = True)
@@ -54,6 +57,14 @@ class AnimeTitle(models.Model):
         vid_list.sort(key=natural_keys)
         return vid_list
 
+    def TrailerVideo(self):
+        if self.trailer is "":
+            Trailer = False
+        else:
+            if "watch?v=" in self.trailer:
+                Trailer = self.trailer.replace("watch?v=","embed/")
+        print(Trailer)
+        return Trailer
 
     def AnimeEpisodes(self):
         file_list = os.listdir(self.directory_address)
